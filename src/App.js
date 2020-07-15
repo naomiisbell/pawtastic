2d160aa9719ba9270360e1cc4d3512b83b02380f
Naomi Isbell <naomiisbell@gmail.com>
2020-07-15 15:55:44 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super()
 
+  }
+
+  render() {
+    return (
+      <body>
+      <div id="parent">
+      <div className="sideOne">
+        <img src="https://www.officialgoldenretriever.com/sites/default/files/resize/upload/dog%20without%20pooping-400x600.jpg"></img>
+      </div>
+      <div className="sideTwo">
+        <h1>Yay! we love dogs! Give us the basics about your pup.</h1>
+        <form className="pet-basics-form">
+          <div className="firstLabel">
+          <label>Name</label>
+          <input
+            type="name"
+            placeholder="Pet's name"
+          />
+          </div>
+
+          <div className="secondLabel">
+          <label>Breed</label>
+          <input
+            type="breed"
+            placeholder="Pet's breed"
+          />
+          </div>
+
+          <div className="thirdLabel">
+          <label>Birthday</label>
+          <input
+            type="birthday"
+            placeholder="MM/DD/YYYY"
+          />
+          </div>
+
+        </form>
+      </div>
+      </div>
+      </body>
+    );
+  }
+};
 export default App;
